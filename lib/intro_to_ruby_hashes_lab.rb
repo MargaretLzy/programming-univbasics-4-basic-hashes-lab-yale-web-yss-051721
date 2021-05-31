8148955f52e5f55204ca4101f48bdc989b349ff2
dynamic-daemon-637b <zl3087@columbia.edu>
2021-05-31 19:13:58 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -5,16 +5,20 @@ end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
-  my={
-    
+  classes={
+    :english =>95
+    :math => 80
+    :art => 90
   }
 end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  hash = {:name => 'Grace Hopper'}
 end
 
 
 def id_hash_generator(number)
   # return a hash with a key :id assigned to the provided number
+  hash ={:id => number}
 end
